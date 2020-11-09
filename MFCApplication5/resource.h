4db6f7143adcdb4ebfa0674300f02b7df65e52ff
BaEmbarrass <3010541967@qq.com>
2020-11-09 23:22:03 +0800

    添加项目文件。

---
+++ MFCApplication5/resource.h
@@ -0,0 +1,28 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCApplication5.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_MFCAPPLICATION5_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDC_num1                        1000
+#define IDC_num2                        1001
+#define IDC_result                      1002
+#define IDC_jian                        1003
+#define IDC_jia                         1004
+#define IDC_cheng                       1005
+#define IDC_RADIO4                      1006
+#define IDC_chu                         1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
